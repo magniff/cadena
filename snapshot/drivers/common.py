d854b0c35b0609e2c2510ba73d4299b03fe6e68e
Alexander Koshkin <akoshkin@ptsecurity.com>
2018-12-23 18:30:36 +0300

    Add proper __repr__ for default linked node class.

--- snapshot/drivers/common.py
+++ snapshot/drivers/common.py
@@ -10,6 +10,11 @@ def sha256_node_id(node):
 
 class DefaultLinkedNode(IdentifiedLinkedNode):
 
+    def __repr__(self):
+        return "%s<data=%s;links=%s>" % (
+            type(self).__qualname__, len(self.data), len(self.links)
+        )
+
     @classmethod
     def from_mapping(cls, mapping, id_maker):
         return cls(id_maker=id_maker, **mapping)
